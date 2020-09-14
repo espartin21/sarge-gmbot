e98568d440c951244b9511e4903b6352a1722114
Ethan Partin <espartin98@gmail.com>
2020-09-14 17:28:01 -0500

    init

---
+++ src/main/java/com/bedtimes/sargegmbot/SargeGmbotApplication.java
@@ -0,0 +1,13 @@
+package com.bedtimes.sargegmbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SargeGmbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SargeGmbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bedtimes/sargegmbot/SargeGmbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bedtimes.sargegmbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SargeGmbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
