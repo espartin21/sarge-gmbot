732b9431cb6afd2a3228674887edc07e3e9d7df7
Ethan Partin <espartin98@gmail.com>
2020-09-14 18:13:00 -0500

    even more init

--- src/main/java/com/bedtimes/sargegmbot/SargeGmbotApplication.java
+++ src/main/java/com/bedtimes/sargegmbot/SargeGmbotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SargeGmbotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SargeGmbotApplication.class, args);
 	}
-
 }
