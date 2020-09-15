c539b4942e52418715d2e9c7a2d022abc421b889
Ethan Partin <espartin98@gmail.com>
2020-09-14 23:26:23 -0500

    oops again

--- src/main/java/com/bedtimes/sargegmbot/callback/controller/CallbackController.java
+++ src/main/java/com/bedtimes/sargegmbot/callback/controller/CallbackController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CallbackController {
     @PostMapping("/callback")
-    public void callback(@RequestBody CallbackData body) {
-        System.out.println("hello");
+    public String callback(@RequestBody CallbackData body) {
+        return body.getName();
     }
 }
