5755493486d24dad9ac02a5595089e6d23ecfc8d
Ethan Partin <espartin98@gmail.com>
2020-09-21 16:35:28 -0500

    Forgot something

--- src/main/java/com/bedtimes/sargegmbot/messenger/service/MessageParserService.java
+++ src/main/java/com/bedtimes/sargegmbot/messenger/service/MessageParserService.java
@@ -1,5 +1,8 @@
 package com.bedtimes.sargegmbot.messenger.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface MessageParserService {
     String parseMessage(String msg);
     // Add your own message parsing function here
