eb4c17c41243c0ce19b1a2bc83b62e93c787e50f
Dawson Fields <dawson@gofields.com>
2020-09-22 13:02:07 -0500

    Add bot mention detection

--- src/main/java/com/bedtimes/sargegmbot/messenger/service/MessageParserServiceImpl.java
+++ src/main/java/com/bedtimes/sargegmbot/messenger/service/MessageParserServiceImpl.java
@@ -1,12 +1,30 @@
 package com.bedtimes.sargegmbot.messenger.service;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageParserServiceImpl implements MessageParserService {
-    public String parseMessage(String msg) {
-        return "";
-    }
+	@Value("${groupme.bot.name}")
+	private String BOT_NAME;
 
-    // Add the same function you made in MessageParserService here and make it private
+	public String parseMessage(String msg) {
+		List<String> tokens = Arrays.asList(msg.split(" "));
+		System.out.println(tokens);
+
+		if(tokens.stream().filter( x -> x.equals("@" + BOT_NAME)).count() > 0){
+			System.out.println("Bot has been mentioned.");
+
+
+
+
+		}
+		return "";
+	}
+
+	// Add the same function you made in MessageParserService here and make it
+	// private
 }
