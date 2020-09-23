814960b127e8b521e5bbce7c2955a8287c2d77df
Dawson Fields <dawson@gofields.com>
2020-09-23 00:26:58 -0500

    Format Help

--- src/main/java/com/bedtimes/sargegmbot/messenger/service/MessageParserServiceImpl.java
+++ src/main/java/com/bedtimes/sargegmbot/messenger/service/MessageParserServiceImpl.java
@@ -31,7 +31,7 @@ public class MessageParserServiceImpl implements MessageParserService {
 		public String execute() {
 			String msg = "Commands:";
 			for (Command cmd : Command.registry) {
-				msg = msg + "\n" + cmd.option.getOpt() + " : " + cmd.option.getDescription();
+				msg = msg + "\n-" + cmd.option.getOpt() + " → " + cmd.option.getDescription();
 			}
 			return msg;
 		}
