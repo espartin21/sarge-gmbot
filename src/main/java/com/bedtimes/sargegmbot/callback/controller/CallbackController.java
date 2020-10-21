09ce7c4c1c88e6c87ae5c4c5d7f2b8611d1a9a48
Ethan Partin <espartin98@gmail.com>
2020-10-21 15:01:07 -0500

    fix styling and remove unused imports

--- src/main/java/com/bedtimes/sargegmbot/callback/controller/CallbackController.java
+++ src/main/java/com/bedtimes/sargegmbot/callback/controller/CallbackController.java
@@ -19,40 +19,41 @@ public class CallbackController {
 	@Value("${groupme.bot.name}")
 	private String BOT_NAME;
   
-  final MessageSenderService messageSenderService;
-  final MessageParserService messageParserService;
-  final GetMembersService getMembersService;
+    final MessageSenderService messageSenderService;
+    final MessageParserService messageParserService;
+    final GetMembersService getMembersService;
 
-  public CallbackController(MessageSenderService messageSenderService, MessageParserService messageParserService, GetMembersService getMembersService) {
-      this.messageSenderService = messageSenderService;
-      this.messageParserService = messageParserService;
-      this.getMembersService = getMembersService;
-  }
+    public CallbackController(MessageSenderService messageSenderService, MessageParserService messageParserService, GetMembersService getMembersService) {
+        this.messageSenderService = messageSenderService;
+        this.messageParserService = messageParserService;
+        this.getMembersService = getMembersService;
+    }
 
 	@PostMapping("/callback")
 	public void callback(@RequestBody CallbackData callbackData) {
-		String senderName = callbackData.getName();
+        String senderName = callbackData.getName();
 
-    // Check that whoever sent the message isn't us (the bot)
-    if (!senderName.equals(BOT_NAME)) {
-        String msg = messageParserService.parseMessage(callbackData);
-        if (msg != null){
-            System.out.println("Command Response: " + msg);
+        // Check that whoever sent the message isn't us (the bot)
+        if (!senderName.equals(BOT_NAME)) {
+            String msg = messageParserService.parseMessage(callbackData);
+            if (msg != null) {
+                System.out.println("Command Response: " + msg);
 
-            ResponseEntity<String> response;
-            if (msg.contains("loci")) { // @ALL - msg is actually the attachments variable
-                List<List<String>> members = getMembersService.getMembers();
-                List<List<String>> membersToMention = members.stream().filter(memberInfo -> !memberInfo.get(0).equals(callbackData.getName())).collect(Collectors.toList());
-                List<String> memberNames = membersToMention.stream().map(memberInfo -> "@" + memberInfo.get(0)).collect(Collectors.toList());
-                String mentionString = String.join(" ", memberNames);
+                ResponseEntity<String> response;
+                if (msg.contains("loci")) { // @ALL - msg is actually the attachments variable
+                    List<List<String>> members = getMembersService.getMembers();
+                    List<List<String>> membersToMention = members.stream().filter(memberInfo -> !memberInfo.get(0).equals(callbackData.getName())).collect(Collectors.toList());
+                    List<String> memberNames = membersToMention.stream().map(memberInfo -> "@" + memberInfo.get(0)).collect(Collectors.toList());
+                    String mentionString = String.join(" ", memberNames);
 
-                response = messageSenderService.sendTextMessage("ATTENTION " + mentionString, msg);
-            } else { // A normal message
-                response = messageSenderService.sendTextMessage(msg);
-            }
+                    response = messageSenderService.sendTextMessage("ATTENTION " + mentionString, msg);
+                } else { // A normal message
+                    response = messageSenderService.sendTextMessage(msg);
+                }
 
-            if (response.getStatusCode() != HttpStatus.ACCEPTED) {
-                 System.out.println("Following message failed to send: " + msg);
+                if (response.getStatusCode() != HttpStatus.ACCEPTED) {
+                    System.out.println("Following message failed to send: " + msg);
+                }
             }
         }
     }
