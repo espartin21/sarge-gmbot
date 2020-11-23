5a19c1a96a2d17860d61ba52899d8e2aebc50837
Ethan Partin <espartin98@gmail.com>
2020-11-23 01:59:20 -0600

    Enable scheduling for app

--- src/main/java/com/bedtimes/sargegmbot/SargeBotDriver.java
+++ src/main/java/com/bedtimes/sargegmbot/SargeBotDriver.java
@@ -7,8 +7,10 @@ import com.bedtimes.sargegmbot.utils.Settings.SettingsDTO;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SargeBotDriver {
 
 	public static Map<String, SettingsDTO> settings;
